1d7a7b52a797fe0ce1db978b5c2b1c7fe666286a
AashifTanwar <otokonokonansensu@gmail.com>
2025-12-11 16:46:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let i = 1;
+
+// setInterval(() => {
+//     document.querySelector('#scroll1').innerHTML += `<img src="post/scroll1/${i}.jpg" alt="">`;
+//     i++;
+//     if( i == 8) {
+//         i = 1;
+//     }
+// }, 1200);
+
+while( i < 8) {
+    document.querySelector('#scroll1').innerHTML += `<img src="post/scroll1/${i}.jpg" alt="img">`;
+    i++;
+}
+i = 1 ;
+while( i < 8) {
+    document.querySelector('#scroll1').innerHTML += `<img src="post/scroll1/${i}.jpg" alt="img">`;
+    i++;
+}
+i = 1 ;
+while( i < 8) {
+    document.querySelector('#scroll1').innerHTML += `<img src="post/scroll1/${i}.jpg" alt="img">`;
+    i++;
+}
